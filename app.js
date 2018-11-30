2dbe8f3844e35243f56537bc75168b72be2cf2ec
kaboul <kaboul777@gmail.com>
2018-11-30 11:05:52 +0600

    Refactoring using router

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const ideas = require ('./routes/ideas');
+const users = require ('./routes/users');
+
+
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost/vidjot-dev',{
@@ -16,8 +20,7 @@ mongoose.connect('mongodb://localhost/vidjot-dev',{
 .then(()=>console.log('MongoDB Connected...'))
 .catch(err =>console.log(err));
 
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -53,84 +56,9 @@ app.get ('/about', (req, res) =>{
     res.render('about');
 });
 
-app.get ('/ideas', (req, res)=>{
-    Idea.find({})
-    .sort({date: 'desc'})
-    .then(ideas => {
-        res.render('ideas/index', {
-            ideas: ideas
-        });
-    });
-});
+app.use('/ideas', ideas);
+app.use('/users', users);
 
-app.get ('/ideas/add', (req, res) =>{
-    res.render('ideas/add');
-});
-
-app.get ('/ideas/edit/:id', (req,res)=>{
-    Idea.findOne({
-        _id: req.params.id
-    })
-    .then (idea =>{
-        res.render('ideas/edit', {
-            idea:idea
-        });
-    })
-});
-
-app.post('/ideas', (req, res)=>{
- let errors = [];
-
- if (!req.body.title){
-     errors.push({text: 'Please add a title'});
- }
- if (!req.body.details){
-     errors.push({text: 'Please add some details'});
- }
-
- if (errors.length>0){
-    res.render ('ideas/add', {
-        errors: errors,
-        title: req.body.title,
-        details:req.body.details
-        });
-    } else {
-      const newUser = {
-        title: req.body.title,
-        details:req.body.details  
-      }  
-        new Idea(newUser)
-        .save()
-        .then(idea => {
-            req.flash('success_msg', 'Video idea added');
-            res.redirect('/ideas');
-        })
-    }
-});
-
-app.put ('/ideas/:id', (req,res) =>{
-    Idea.findOne({
-        _id: req.params.id
-    })
-    .then(idea=>{
-        idea.title = req.body.title;
-        idea.details =req.body.details;
-
-        idea.save()
-        .then(idea=>{
-            req.flash('success_msg', 'Video idea updated');
-            res.redirect('/ideas');
-        })
-    });
-});
-
-app.delete('/ideas/:id', (req,res)=> {
-    Idea.remove ({_id: req.params.id})
-    .then (()=> {
-        req.flash('success_msg', 'Video idea removed');
-        res.redirect ('/ideas');
-    });
-});
 
 const port = 5000;
 app.listen(port, ()=>{
