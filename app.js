461b220936d4b2683a220a47db30fef401494c92
kaboul <kaboul777@gmail.com>
2018-12-02 19:05:27 +0600

    Adding Authentication

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require ('path');
 const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require ('connect-flash');
 const session = require ('express-session');
 const bodyParser = require('body-parser');
+const passport = require ('passport');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -11,6 +13,7 @@ const app = express();
 const ideas = require ('./routes/ideas');
 const users = require ('./routes/users');
 
+require ('./config/passport')(passport);
 
 mongoose.Promise = global.Promise;
 
@@ -28,6 +31,8 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use (express.static(path.join(__dirname, 'public')));
+
 app.use(methodOverride('_method'));
 
 app.use(session({
@@ -36,12 +41,16 @@ app.use(session({
     saveUninitialized: true
   }));
 
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 app.use (flash());
 
 app.use (function (req,res,next){
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
